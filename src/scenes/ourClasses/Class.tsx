7ad23e6a4ef408dd90611fc292cc6521467c8702
Xeeshan <xeeshann.ahsan@gmail.com>
2025-09-13 18:16:35 +0600

    fixing

--- src/scenes/ourClasses/Class.tsx
+++ src/scenes/ourClasses/Class.tsx
@@ -1,9 +1,9 @@
 type Props = {
   name: string;
-  descripton?: string;
+  description?: string;
   image: string;
 };
-const Class = ({ name, descripton, image }: Props) => {
+const Class = ({ name, description, image }: Props) => {
   const overlayStyles = `p-5 absolute z-30 flex
   h-[380px] w-[450px] flex-col items-center justify-center
   whitespace-normal bg-primary-500 text-center text-white
@@ -13,7 +13,7 @@ const Class = ({ name, descripton, image }: Props) => {
     <li className="relative mx-5 inline-block h-[380px] w-[450px]">
       <div className={overlayStyles}>
         <p className="text-2xl">{name}</p>
-        <p className="mt-5">{descripton}</p>
+        <p className="mt-5">{description}</p>
       </div>
       <img src={image} alt={`${image}`} />
     </li>
